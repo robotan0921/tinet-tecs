0a639bc646e692420a5eb15d0d050a253f5d3604
robotan0921 <robotan0921@gmail.com>
2017-01-15 18:27:55 +0900

    Implement the function of tEthernetOutput

--- tinet/net/tEthernetOutput.c
+++ tinet/net/tEthernetOutput.c
@@ -45,6 +45,12 @@
  *   ER             cArpOutput_arpResolve_outputp_reuse( void* buf );
  *   ER_UINT        cArpOutput_arpResolve_outputp_bufferSize( const void* buf );
  *   uint32_t       cArpOutput_arpResolve_outputp_bufferMaxSize( );
+ * allocator port for call port:eEthernetOutput func:ethernetOutput param: outputp
+ *   ER             eEthernetOutput_ethernetOutput_outputp_alloc( void** buf, const int32_t minlen, TMO tmout );
+ *   ER             eEthernetOutput_ethernetOutput_outputp_dealloc( const void* buf );
+ *   ER             eEthernetOutput_ethernetOutput_outputp_reuse( void* buf );
+ *   ER_UINT        eEthernetOutput_ethernetOutput_outputp_bufferSize( const void* buf );
+ *   uint32_t       eEthernetOutput_ethernetOutput_outputp_bufferMaxSize( );
  *
  * #[</PREAMBLE>]# */
 
@@ -56,7 +62,91 @@
 #define	E_ID	(-18)	/* illegal ID */
 #endif
 
-//TODO: Add the entry function (Should apply tecsmerge)
+#include <string.h>
+
+#ifdef TARGET_KERNEL_ASP
+
+#include <kernel.h>
+#include <sil.h>
+#include <t_syslog.h>
+#include "kernel_cfg.h"
+
+#endif	/* of #ifdef TARGET_KERNEL_ASP */
+
+#ifdef TARGET_KERNEL_JSP
+
+#include <s_services.h>
+#include <t_services.h>
+#include "kernel_id.h"
+
+#endif	/* of #ifdef TARGET_KERNEL_JSP */
+
+#include <tinet_defs.h>
+#include <tinet_config.h>
+
+#include <net/if.h>
+#include <net/ethernet.h>
+#include <net/if_llc.h>
+#include <net/if_arp.h>
+#include <net/net.h>
+#include <net/net_var.h>
+#include <net/net_buf.h>
+#include <net/net_timer.h>
+#include <net/net_count.h>
+
+#include <netinet/in.h>
+#include <netinet/in_var.h>
+#include <netinet/if_ether.h>
+
+#include <netinet6/if6_ether.h>
+#include <netinet6/nd6.h>
+
+#include <net/if_var.h>
+
+/* #[<ENTRY_PORT>]# eEthernetOutput
+ * entry port: eEthernetOutput
+ * signature:  sEthernetOutput
+ * context:    task
+ * #[</ENTRY_PORT>]# */
+
+/* #[<ENTRY_FUNC>]# eEthernetOutput_ethernetOutput
+ * name:         eEthernetOutput_ethernetOutput
+ * global_name:  tEthernetOutput_eEthernetOutput_ethernetOutput
+ * oneway:       false
+ * #[</ENTRY_FUNC>]# */
+ER
+eEthernetOutput_ethernetOutput(CELLIDX idx, int8_t* outputp, int32_t size, T_IN4_ADDR dstaddr, TMO tmout)
+{
+	ER		ercd = E_OK;
+	CELLCB	*p_cellcb;
+	if (VALID_IDX(idx)) {
+		p_cellcb = GET_CELLCB(idx);
+	}
+	else {
+		return(E_ID);
+	} /* end if VALID_IDX(idx) */
+
+	/* ここに処理本体を記述します #_TEFB_# */
+	T_NET_BUF *output = (T_NET_BUF*)outputp;
+	uint8_t mymac[6];
+	cNicDriver_probe(mymac);
+
+	/*ヘッダのセット */
+	memcpy(GET_ETHER_HDR(output)->shost, mymac, 6);
+
+	if(output->off.protocolflag & FLAG_USE_IPV4) {
+	GET_ETHER_HDR(output)->type = htons(ETHER_TYPE_IP);
+		if(is_cArpOutput_joined()){}
+			//TODO: return cArpOutput_arpResolve(outputp, size, dstaddr, mymac, tmout);
+	}
+	else {
+
+		eEthernetOutput_ethernetOutput_outputp_dealloc((void*)outputp);
+		return E_PAR;
+	}
+
+	return(ercd);
+}
 
 /* #[<POSTAMBLE>]#
  *   これより下に非受け口関数を書きます
