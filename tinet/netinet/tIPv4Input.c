fe253aae573616fd7be8c1527fe574603e60920c
robotan0921 <robotan0921@gmail.com>
2017-10-20 20:40:21 +0900

    Remove TODO

--- tinet/netinet/tIPv4Input.c
+++ tinet/netinet/tIPv4Input.c
@@ -326,19 +326,20 @@ eIPv4Input_IPv4Input(CELLIDX idx, int8_t* inputp, int32_t size)
 #if defined(SUPPORT_UDP) && ( (TNUM_UDP4_CEPID > 0) || \
                              ((TNUM_UDP6_CEPID > 0) && defined(API_CFG_IP4MAPPED_ADDR)))
 	case IPPROTO_UDP:
-	//TODO: if (is_cUDPInput_joined()) {
-		NET_COUNT_MIB(ip_stats.ipInDelivers, 1);
-		// udp4_input(&input, &off, NULL);
-		cUDPInput_UDPInput(inputp, size, (int8_t *)&dst, 4); // dstはホストオーダ順で入っている[0]=200 [1]=1 [2]=168 [3]=192//offmikan
-		return;
+		if (is_cUDPInput_joined()) {
+			NET_COUNT_MIB(ip_stats.ipInDelivers, 1);
+			cUDPInput_UDPInput(inputp, size, (int8_t *)&dst, 4); // dstはホストオーダ順で入っている[0]=200 [1]=1 [2]=168 [3]=192//offmikan
+			return;
+		}
 		break;
 #endif	/* of #if defined(SUPPORT_UDP) && TNUM_UDP4_CEPID > 0 */
 
 	case IPPROTO_ICMP:
-		//TODO: if (is_cICMP4_joined()) {
-		NET_COUNT_MIB(ip_stats.ipInDelivers, 1);
-		cICMP4_input(inputp, off);	//TODO: off = size ??
-		return;
+		if (is_cICMP4_joined()) {
+			NET_COUNT_MIB(ip_stats.ipInDelivers, 1);
+			cICMP4_input(inputp, off);	//TODO: off = size ??
+			return;
+		}
 		break;
 
 #if defined(SUPPORT_IGMP)
